f21bd914c219fd03e0ee3129dd2237db50f6e293
Arnaud293 <arnaudlt293@gmail.com>
2022-09-20 12:17:55 +0200

    add end scroll pop-up with closing event

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const nav = document.querySelector('nav');
+const popUp = document.querySelector('.pop-up')
 
 let scrollPosition = 0;
 
@@ -16,6 +17,16 @@ window.addEventListener('scroll', () => {
    scrollPosition = window.scrollY;
 })
 
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 850 ){
+        popUp.style.transform = 'translateX(0px)'
+        popUp.style.opacity = 1;
+    }
+})
+
+closeBtn.addEventListener('click', () => {
+    popUp.style.display = 'none';
+})
 
 // Faire apparaitre l'image de la partie improvise
 
