6ab56955b0e30b41b15ca083605a99720d01fefc
Arnaud293 <arnaudlt293@gmail.com>
2022-09-20 11:43:07 +0200

    simple img transition on scroll

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-// Créer un événement au scroll
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+const nav = document.querySelector('nav');
+
+let scrollPosition = 0;
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > scrollPosition){
+        nav.style.height = 50 + 'px';
+        imgImprovise.style.opacity = 1;
+    }
+    else{
+        nav.style.height = 80 + 'px';
+        imgImprovise.style.opacity = 0;
+    }
+
+   scrollPosition = window.scrollY;
+})
+
 
 // Faire apparaitre l'image de la partie improvise
 
